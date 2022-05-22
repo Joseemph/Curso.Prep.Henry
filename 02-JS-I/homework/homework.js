8995128144082c1cbc1fb15b4ff3140a3d6fcb01
Joseemph <joseemph1598@gmail.com>
2022-05-22 01:19:51 -0400

    Finish work 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -79,14 +79,16 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num < 90
+  if(num<90) return true
+  else return false
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num >50
+  if(num>50) return true
+  else return false
 }
 
 function obtenerResto(x, y) {
